3096767aecc7a34dbb9e7fb3e87d7d421a924b0c
Shiawase1099 <shiawase1099@mail.ru>
2026-06-23 20:13:08 +0900

    scss

---
+++ src/js/index.js
@@ -0,0 +1,28 @@
+import '../scss/style.scss'
+
+console.log('It works!')
+
+const button = document.querySelector('.show-more')
+const buttonText = document.querySelector('.show-more-text')
+const grid = document.querySelector('.brands-grid')
+
+button.addEventListener('click', () => {
+  grid.classList.toggle('open')
+  button.classList.toggle('active')
+
+  if (grid.classList.contains('open')) {
+    buttonText.textContent = 'Скрыть'
+  } else {
+    buttonText.textContent = 'Показать все'
+  }
+})
+
+new Swiper('.swiper', {
+  slidesPerView: 'auto',
+  spaceBetween: 16,
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  }
+})
